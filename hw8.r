7b388b4b76fb6d872f1d2aa558e2fe41c0fdd1aa
timjun <timnuj@gmail.com>
2016-04-20 03:46:06 -0400

    Update hw8.r

--- hw8.r
+++ hw8.r
@@ -1,10 +1,8 @@
 
 
-
-
-
 diets <- read.csv("C:/Users/tim/Desktop/diets.txt", header=FALSE)
 
+# initializing, couldnt get the for loop to work otherwise...
 sd=0
 dbar=0
 n=length(diets[,1])
@@ -14,10 +12,12 @@ for (i in 1:4) {
   dbar[i] = mean(diets[,i])
 }
 
+# these prior hyperparms were given in the problem
 sig = mean(sd)
 mu = mean(dbar)
 tau = sd(dbar)
 
+# N-N updating
 mu1 = (mu/tau^2 + sum(diets[,1])/sig^2) / (1/tau^2 + n/sig^2)
 mu2 = (mu/tau^2 + sum(diets[,2])/sig^2) / (1/tau^2 + n/sig^2)
 mu3 = (mu/tau^2 + sum(diets[,3])/sig^2) / (1/tau^2 + n/sig^2)
@@ -28,6 +28,7 @@ tau2 = (1/tau^2 + n/sig^2)^-.5
 tau3 = (1/tau^2 + n/sig^2)^-.5
 tau4 = (1/tau^2 + n/sig^2)^-.5
 
+# 95% credible intervals
 qnorm(c(.025,.975),mu1,tau1)
 qnorm(c(.025,.975),mu2,tau2)
 qnorm(c(.025,.975),mu3,tau3)
